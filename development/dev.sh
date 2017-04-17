cc68a34bb4582c1f5844dd37b09b9e465a6cf04a
caiomcg <caiomcg@gmail.com>
2017-04-17 11:53:18 -0300

    Add vim and wireshark to dev essentials

--- development/dev.sh
+++ development/dev.sh
@@ -4,6 +4,14 @@ APT_FLAGS="--quiet --assume-yes"
 
 sudo apt $APT_FLAGS update
 
+if (whiptail --title "Linux Backup - caiomcg@dunadan" --yesno "Install vim?" 8 78) then
+    sudo apt install $APT_FLAGS vim
+fi
+
+if (whiptail --title "Linux Backup - caiomcg@dunadan" --yesno "Install Wireshark?" 8 78) then
+    sudo apt install $APT_FLAGS wireshark
+fi
+
 if (whiptail --title "Linux Backup - caiomcg@dunadan" --yesno "Install Terminator?" 8 78) then
     sudo apt install $APT_FLAGS terminator
 fi
